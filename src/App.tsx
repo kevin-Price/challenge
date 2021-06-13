6559611335502b99609b0dc132e4642759110dd2
kevin <kaprice919@gmail.com>
2021-06-13 13:45:08 -0700

    Added a new tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import Page from './components/page';
 import Game from './components/game';
 
 const App: React.FC = () => {
   return (
     <Page>
+      <Page>
+
+      </Page>
       <Game />
     </Page>
   );
